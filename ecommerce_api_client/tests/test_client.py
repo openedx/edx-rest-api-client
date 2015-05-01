9ad5fe44304bf5203f1cc139edd1672688949f78
jsa <jsa@edx.org>
2015-05-01 15:32:41 -0400

    support tracking_context in JWT token.
    XCOM-208

--- ecommerce_api_client/tests/test_client.py
+++ ecommerce_api_client/tests/test_client.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import ddt
+import mock
 
 from ecommerce_api_client.client import EcommerceApiClient
 
@@ -9,6 +10,7 @@ URL = 'http://example.com/api/v2'
 SIGNING_KEY = 'edx'
 USERNAME = 'edx'
 EMAIL = 'edx@example.com'
+TRACKING_CONTEXT = {'foo': 'bar'}
 
 
 @ddt.ddt
@@ -28,3 +30,9 @@ class EcommerceApiClientTests(TestCase):
     def test_invalid_configuration(self, args):
         """ If the constructor arguments are invalid, an InvalidConfigurationError should be raised. """
         self.assertRaises(ValueError, EcommerceApiClient, *args)
+
+    @mock.patch('ecommerce_api_client.auth.JwtAuth.__init__', return_value=None)
+    def test_tracking_context(self, mock_auth):
+        """ Ensure the tracking context is included with API requests if specified. """
+        EcommerceApiClient(URL, SIGNING_KEY, USERNAME, EMAIL, tracking_context=TRACKING_CONTEXT)
+        self.assertDictContainsSubset(mock_auth.call_args[1], TRACKING_CONTEXT)
