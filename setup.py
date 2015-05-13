358b74dbf017e9843b0be8f304f672815213a816
jsa <jsa@edx.org>
2015-05-13 18:15:49 -0400

    update slumber fork and dependency declaration.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='ecommerce-api-client',
-    version='0.4.1',
+    version='0.4.2',
     packages=['ecommerce_api_client'],
     url='https://github.com/edx/ecommerce-api-client',
     description='Client used to access edX E-Commerce Service',
     long_description=open('README.rst').read(),
     install_requires=[
-        'slumber==0.7.0',
+        # NOTE this version of slumber only exists on the edx fork.
+        # dependency_links is deprecated and no longer works, so any packages
+        # depending on this package are responsible for manually installing
+        # this requirement before this package can be installed.
+        'slumber==0.7.1-decode-utf8',
     ],
     tests_require=[
         'coverage==3.7.1',
