db45f2f37490d1fadd683b9d240fcee362bdd18b
Jawayria <jawayriahashmi@gmail.com>
2021-07-08 20:40:47 +0500

    feat: Added support for django3.2

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.8',
+        'Framework :: Django :: 3.2',
         'Topic :: Internet',
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
